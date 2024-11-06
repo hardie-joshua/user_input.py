05bf5a917245da059eaeb1dd1f9e0a23b1fb5095
Joshua Mutinda <mjoshuaofficial@gmail.com>
2024-11-06 23:26:04 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Get user input
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where are you from? ")
+
+# Print personalized message
+print(f"Hello {Joshua}! You are {21} years old and from {Nairobi}.")
